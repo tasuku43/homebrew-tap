a1cd8f51356ce7ad3f6d097661a089760cde3773
tasuku43 <43329036+tasuku43@users.noreply.github.com>
2026-06-19 10:36:20 +0000

    Update Homebrew formula for v0.5.15

--- Formula/kra.rb
+++ Formula/kra.rb
@@ -3,25 +3,25 @@ class Kra < Formula
   homepage "https://github.com/tasuku43/kra"
   license "MIT"
 
-  version "0.5.14"
+  version "0.5.15"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/tasuku43/kra/releases/download/v0.5.14/kra_v0.5.14_macos_arm64.tar.gz"
-      sha256 "f176ac0badbe675db73fec9af3bbed3db5bb7705b4301ecee3b3056fcdc3f9d3"
+      url "https://github.com/tasuku43/kra/releases/download/v0.5.15/kra_v0.5.15_macos_arm64.tar.gz"
+      sha256 "46399e24980970b0bfb6d8879579c967c324c29614e9c0fdbbae01f06b79a637"
     else
-      url "https://github.com/tasuku43/kra/releases/download/v0.5.14/kra_v0.5.14_macos_x64.tar.gz"
-      sha256 "753bf6e19beca5d18797666811278ac7be885ea1ea16d7c68eb798c307fcd09e"
+      url "https://github.com/tasuku43/kra/releases/download/v0.5.15/kra_v0.5.15_macos_x64.tar.gz"
+      sha256 "504bc1737e71801d7425ecba3972efd2292037519c2c7022dd7da0505818a10d"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/tasuku43/kra/releases/download/v0.5.14/kra_v0.5.14_linux_arm64.tar.gz"
-      sha256 "80c906d80efc1fda488d6d2274d47d15bd47351fb5b56faaf67ddd16d2b396cf"
+      url "https://github.com/tasuku43/kra/releases/download/v0.5.15/kra_v0.5.15_linux_arm64.tar.gz"
+      sha256 "abfdce73517f98da8f2c4acabba5258143e5cc8da47cc8e58251ab3b9f5e38d1"
     else
-      url "https://github.com/tasuku43/kra/releases/download/v0.5.14/kra_v0.5.14_linux_x64.tar.gz"
-      sha256 "8676f224b9ff76ec71ccc9f455bd5543418b5888d84954ff24a46651c72b1a80"
+      url "https://github.com/tasuku43/kra/releases/download/v0.5.15/kra_v0.5.15_linux_x64.tar.gz"
+      sha256 "e3f07026f023e4437b00d2ef3eb2e4819e15ab4f5fb46e31ed7175e9acc9487d"
     end
   end
 
@@ -33,4 +33,3 @@ class Kra < Formula
     system "#{bin}/kra", "version"
   end
 end
-
