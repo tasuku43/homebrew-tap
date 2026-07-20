2b44ca3904383c6de50424b66f2f52151a7610ab
tasuku43 <43329036+tasuku43@users.noreply.github.com>
2026-07-20 09:28:45 +0000

    Update Homebrew formula for v0.2.2

--- Formula/cwk.rb
+++ Formula/cwk.rb
@@ -1,16 +1,16 @@
 class Cwk < Formula
   desc "Chatworkをタスク単位で操作するCLI。"
   homepage "https://github.com/tasuku43/cwk"
-  version "0.2.1"
+  version "0.2.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/tasuku43/cwk/releases/download/v0.2.1/cwk_v0.2.1_darwin_arm64.tar.gz"
-      sha256 "18769bd3dc757779d969800668bc468a7467667521ba1a48b3a460ad3b88cf05"
+      url "https://github.com/tasuku43/cwk/releases/download/v0.2.2/cwk_v0.2.2_darwin_arm64.tar.gz"
+      sha256 "28ccf468923cb92a48e447b2009723eec77148b2494220b0e184c03d2f6eed62"
     else
-      url "https://github.com/tasuku43/cwk/releases/download/v0.2.1/cwk_v0.2.1_darwin_amd64.tar.gz"
-      sha256 "7c0ca89075d6616f6815fc3e764b6d33ac456db8d9f407296ae3c237c7287ee4"
+      url "https://github.com/tasuku43/cwk/releases/download/v0.2.2/cwk_v0.2.2_darwin_amd64.tar.gz"
+      sha256 "b5b52d7240cfbc9cab04c993b745f828e4a76a52a0b782f04cf2cbb4d5f60dd0"
     end
   end
 
