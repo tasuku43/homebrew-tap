9f72529eddeb160ba910f5e54191738e2648b7b0
tasuku43 <43329036+tasuku43@users.noreply.github.com>
2026-07-21 13:19:34 +0000

    Update Homebrew formula for v0.2.19

--- Formula/cc-bash-guard.rb
+++ Formula/cc-bash-guard.rb
@@ -3,25 +3,25 @@ class CcBashGuard < Formula
   homepage "https://github.com/tasuku43/cc-bash-guard"
   license "MIT"
 
-  version "0.2.18"
+  version "0.2.19"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/tasuku43/cc-bash-guard/releases/download/v0.2.18/cc-bash-guard_v0.2.18_macos_arm64.tar.gz"
-      sha256 "0e08fd78e38300837d197d4373aee4ba233b333fd96deea54edfedb038b6c96b"
+      url "https://github.com/tasuku43/cc-bash-guard/releases/download/v0.2.19/cc-bash-guard_v0.2.19_macos_arm64.tar.gz"
+      sha256 "669013dcb1bccc88fc636b4590e60a91ddfbf7680d3fd41473b4b75f870096b5"
     else
-      url "https://github.com/tasuku43/cc-bash-guard/releases/download/v0.2.18/cc-bash-guard_v0.2.18_macos_x64.tar.gz"
-      sha256 "2c20063aed3859687d64cbdaddf50f6d7609ea6066bfb7bfe44cd66b20d7b6a5"
+      url "https://github.com/tasuku43/cc-bash-guard/releases/download/v0.2.19/cc-bash-guard_v0.2.19_macos_x64.tar.gz"
+      sha256 "80201cca704b42cab7122d456201b4f3b16de7d79a90b0964419e6baf9f84e71"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/tasuku43/cc-bash-guard/releases/download/v0.2.18/cc-bash-guard_v0.2.18_linux_arm64.tar.gz"
-      sha256 "a1a411e53db9973c2e7ab3f616222d7a3708134dafa923d418e85073fe444d1a"
+      url "https://github.com/tasuku43/cc-bash-guard/releases/download/v0.2.19/cc-bash-guard_v0.2.19_linux_arm64.tar.gz"
+      sha256 "171cbdde87381d626832d106ef6b50486224fb46cbf3fe2d5572bbf6dec5ec18"
     else
-      url "https://github.com/tasuku43/cc-bash-guard/releases/download/v0.2.18/cc-bash-guard_v0.2.18_linux_x64.tar.gz"
-      sha256 "0da13c1116f95d1bee523055a61ad3638d1931ec94fc93d0b7bfbf86185c5768"
+      url "https://github.com/tasuku43/cc-bash-guard/releases/download/v0.2.19/cc-bash-guard_v0.2.19_linux_x64.tar.gz"
+      sha256 "481d4276c1c3e0ad72b803649e47b705eb52ad1af715a0ffbb003487e8031026"
     end
   end
 
